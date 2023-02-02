7ba5cc0b69adfdebd78a5a6bd81c567a3fe65c23
Olga Pomytkina <olpoma@yandex.ru>
2023-02-02 23:35:13 +0300

    feat: реализовать переключение на английский язык

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+// Переключение страницы на английский язык
+
+document.getElementById('english').onclick = function () {
+  window.location.href = 'engindex.html';
+};
+
+// Переключение страницы на русский язык
+
+document.getElementById('russian').onclick = function () {
+  window.location.href = 'index.html';
+};
